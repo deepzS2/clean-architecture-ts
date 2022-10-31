1a74eb8441559c48230cba49b86b09e1e87878bf
deepzS2 <alanbackman10@hotmail.com>
2022-10-31 16:41:32 -0300

    feat: ensure SignUpController returns 500 if EmailValidator throws

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,6 @@
 import { InvalidParamError } from '../errors/invalid-param-error'
 import { MissingParamError } from '../errors/missing-param-error'
+import { ServerError } from '../errors/server-error'
 import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
@@ -11,21 +12,28 @@ export class SignUpController implements Controller {
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+    try {
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
-    }
 
-    const isValidEmail = this._emailValidator.isValid(httpRequest.body.email)
+      const isValidEmail = this._emailValidator.isValid(httpRequest.body.email)
 
-    if (!isValidEmail) return badRequest(new InvalidParamError('email'))
+      if (!isValidEmail) return badRequest(new InvalidParamError('email'))
 
-    return {
-      statusCode: 200,
-      body: httpRequest.body
+      return {
+        statusCode: 200,
+        body: httpRequest.body
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError()
+      }
     }
   }
 }
---
+++ src/presentation/errors/server-error.ts
@@ -0,0 +1,7 @@
+export class ServerError extends Error {
+  constructor () {
+    super('Internal Server Error')
+
+    this.name = 'ServerError'
+  }
+}
