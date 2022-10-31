b803931e36c33f3f5c679754b62b37deff346b7a
deepzS2 <alanbackman10@hotmail.com>
2022-10-31 16:01:44 -0300

    test: ensure SignUpController calls EmailValidator with correct email

--- src/presentation/controllers/signup.test.ts
+++ src/presentation/controllers/signup.test.ts
@@ -110,4 +110,22 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new InvalidParamError('email'))
   })
+
+  it('Should call EmailValidator with correct email', () => {
+    const { sut, emailValidatorStub } = makeSut()
+
+    const isValidSpy = vitest.spyOn(emailValidatorStub, 'isValid')
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'invalid_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    sut.handle(httpRequest)
+    expect(isValidSpy).toHaveBeenCalledWith('invalid_email@mail.com')
+  })
 })
