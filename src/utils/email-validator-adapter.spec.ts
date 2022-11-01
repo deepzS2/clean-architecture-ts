9d0a9db040c7857982779bfdcdceeb0e84ed037b
deepzS2 <alanbackman10@hotmail.com>
2022-11-01 11:38:56 -0300

    test: ensure EmailValidatorAdapter returns true if validator returns true

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,11 +1,33 @@
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, vitest } from 'vitest'
 import { EmailValidatorAdapter } from './email-validator-adapter'
+import validator from 'validator'
+
+vitest.mock('validator', async () => {
+  const actual = await import('validator')
+
+  const mocked = {
+    ...actual,
+    isEmail (): boolean {
+      return true
+    }
+  }
+
+  return mocked
+})
 
 describe('EmailValidator Adapter', () => {
   it('Should return false if validator returns false', () => {
     const sut = new EmailValidatorAdapter()
+    vitest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
 
     const isValidEmail = sut.isValid('invalid_email@mail.com')
     expect(isValidEmail).not.toBeTruthy()
+  })
+
+  it('Should return false if validator returns true', () => {
+    const sut = new EmailValidatorAdapter()
+
+    const isValidEmail = sut.isValid('valid_email@mail.com')
+    expect(isValidEmail).toBeTruthy()
   })
 })
