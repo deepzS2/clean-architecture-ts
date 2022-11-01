5b64dbd8e5a6141fd19ebcf110680381b86960f3
deepzS2 <alanbackman10@hotmail.com>
2022-11-01 15:21:12 -0300

    test: ensure BcryptAdapter returns a hash on success

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,15 +1,31 @@
 import { describe, expect, it, vi } from 'vitest'
-import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
+import bcrypt from 'bcrypt'
+
+vi.mock('bcrypt', async () => ({
+  default: {
+    async hash (data: string, saltOrRounds: number): Promise<string> {
+      return await new Promise(resolve => resolve('hash'))
+    }
+  }
+}))
 
 const makeSut = (salt: number): BcryptAdapter => new BcryptAdapter(salt)
 
 describe('Bcrypt Adapter', () => {
   it('Should call bcrypt with correct values', async () => {
-    const sut = makeSut(12)
+    const salt = 12
+    const sut = makeSut(salt)
     const hashSpy = vi.spyOn(bcrypt, 'hash')
 
     await sut.encrypt('any_value')
-    expect(hashSpy).toHaveBeenCalledWith('any_value', 12)
+    expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  it('Should return a hash on success', async () => {
+    const sut = makeSut(12)
+
+    const hash = await sut.encrypt('any_value')
+    expect(hash).toBe('hash')
   })
 })
