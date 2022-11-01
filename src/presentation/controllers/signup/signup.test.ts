2b24d3bfde186598a904ba30f16f14638884cab3
deepzS2 <alanbackman10@hotmail.com>
2022-10-31 21:13:22 -0300

    test: ensure SignUpController returns 500 if AddAccount throws

--- src/presentation/controllers/signup/signup.test.ts
+++ src/presentation/controllers/signup/signup.test.ts
@@ -212,4 +212,26 @@ describe('SignUp Controller', () => {
       password: 'any_password'
     })
   })
+
+  it('Should return 500 if AddAccount throws', () => {
+    const { sut, addAccountStub } = makeSut()
+
+    vitest.spyOn(addAccountStub, 'add').mockImplementationOnce(() => {
+      throw new Error()
+    })
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'invalid_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
+  })
 })
