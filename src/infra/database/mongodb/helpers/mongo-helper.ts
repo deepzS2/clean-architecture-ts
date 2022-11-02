d51e518345e31d5e1fa9d5da8cabcc4b4a2aa2c6
deepzS2 <alanbackman10@hotmail.com>
2022-11-02 13:47:44 -0300

    feat: ensure AccountMongoRepository returns account on success

--- src/infra/database/mongodb/helpers/mongo-helper.ts
+++ src/infra/database/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient, Document } from 'mongodb'
 
 export class MongoHelper {
   public static client: MongoClient
@@ -9,5 +9,9 @@ export class MongoHelper {
 
   static async disconnect (): Promise<void> {
     await this.client.close()
+  }
+
+  static getCollection<T extends Document> (name: string): Collection<T> {
+    return this.client.db().collection<T>(name)
   }
 }
