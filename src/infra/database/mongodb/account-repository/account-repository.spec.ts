785672d1c2eac294286e427f3fa048d82a3e68b4
deepzS2 <alanbackman10@hotmail.com>
2022-11-02 13:49:02 -0300

    test: ensure AccountMongoRepository returns account on success

---
+++ src/infra/database/mongodb/account-repository/account-repository.spec.ts
@@ -0,0 +1,29 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { AccountMongoRepository } from './account-repository'
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(globalThis.__MONGO_URI__)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  it('Should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@mail.com')
+    expect(account.password).toBe('any_password')
+  })
+})
