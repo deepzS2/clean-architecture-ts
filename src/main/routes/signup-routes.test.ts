d81aa29c00c98b199978ce11243d875844239ab4
deepzS2 <alanbackman10@hotmail.com>
2022-11-03 10:16:45 -0300

    test: ensure SignUp route returns an account on success

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,14 @@
+import { it, describe } from 'vitest'
+import request from 'supertest'
+import app from '../config/app'
+
+describe('SignUp Routes', () => {
+  it('Should return an account on success', async () => {
+    await request(app).post('/api/signup').send({
+      name: 'Alan',
+      email: 'alanr.developer@hotmail.com',
+      password: '123',
+      passwordConfirmation: '123'
+    }).expect(200)
+  })
+})
