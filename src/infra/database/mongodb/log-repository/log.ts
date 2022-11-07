fe8276e420fdc40cabd0485bf5c86be315a5c955
deepzS2 <alanbackman10@hotmail.com>
2022-11-07 12:07:34 -0300

    feat: ensure LogMongoRepository creates an error log on success

---
+++ src/infra/database/mongodb/log-repository/log.ts
@@ -0,0 +1,13 @@
+import { LogErrorRepository } from '../../../../data/protocols/log-error-repository'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class LogMongoRepository implements LogErrorRepository {
+  async logError (stack: string): Promise<void> {
+    const errorsCollection = await MongoHelper.getCollection('errors')
+
+    await errorsCollection.insertOne({
+      stack,
+      date: new Date()
+    })
+  }
+}
--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -9,7 +9,7 @@ export class LogControllerDecorator implements Controller {
     const httpResponse = await this._controller.handle(httpRequest)
 
     if (httpResponse.statusCode === 500) {
-      await this._logErrorRepository.log(httpResponse.body.stack)
+      await this._logErrorRepository.logError(httpResponse.body.stack)
     }
 
     return httpResponse
