6d5c0c89d845700cf6e7f450a500704942331151
deepzS2 <alanbackman10@hotmail.com>
2022-11-09 15:56:40 -0300

    test: ensure ValidationComposite returns the first error if more then one validation fails

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -5,7 +5,7 @@ import { ValidationComposite } from './validation-composite'
 
 interface SutTypes {
   sut: ValidationComposite
-  validationStub: Validation
+  validationStubs: Validation[]
 }
 
 const makeValidation = (): Validation => {
@@ -19,21 +19,30 @@ const makeValidation = (): Validation => {
 }
 
 const makeSut = (): SutTypes => {
-  const validationStub = makeValidation()
-  const sut = new ValidationComposite(validationStub)
+  const validationStubs = [makeValidation(), makeValidation()]
+  const sut = new ValidationComposite(...validationStubs)
 
   return {
     sut,
-    validationStub
+    validationStubs
   }
 }
 
 describe('Validation Composite', () => {
   it('Should return an error if any validation fails', () => {
-    const { sut, validationStub } = makeSut()
-    vi.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('field'))
+    const { sut, validationStubs } = makeSut()
+    vi.spyOn(validationStubs[1], 'validate').mockReturnValueOnce(new MissingParamError('field'))
 
     const result = sut.validate({ field: 'any_value' })
     expect(result).toEqual(new MissingParamError('field'))
+  })
+
+  it('Should return the first error if more then one validation fails', () => {
+    const { sut, validationStubs } = makeSut()
+    vi.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(new Error())
+    vi.spyOn(validationStubs[1], 'validate').mockReturnValueOnce(new MissingParamError('field'))
+
+    const result = sut.validate({ field: 'any_value' })
+    expect(result).toEqual(new Error())
   })
 })
