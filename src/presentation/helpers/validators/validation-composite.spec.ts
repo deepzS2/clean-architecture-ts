5dca702c598a18b9540fa633e6d8baeec78b6e93
deepzS2 <alanbackman10@hotmail.com>
2022-11-09 15:54:33 -0300

    refactor: move sut creation to a factory method

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -1,21 +1,37 @@
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 import { MissingParamError } from '../../errors'
 import { Validation } from './validation'
 import { ValidationComposite } from './validation-composite'
 
-const makeSut = (): ValidationComposite => {
+interface SutTypes {
+  sut: ValidationComposite
+  validationStub: Validation
+}
+
+const makeValidation = (): Validation => {
   class ValidationStub implements Validation {
     validate (input: any): Error | null {
-      return new MissingParamError('field')
+      return null
     }
   }
 
-  return new ValidationComposite(new ValidationStub())
+  return new ValidationStub()
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidation()
+  const sut = new ValidationComposite(validationStub)
+
+  return {
+    sut,
+    validationStub
+  }
 }
 
 describe('Validation Composite', () => {
   it('Should return an error if any validation fails', () => {
-    const sut = makeSut()
+    const { sut, validationStub } = makeSut()
+    vi.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('field'))
 
     const result = sut.validate({ field: 'any_value' })
     expect(result).toEqual(new MissingParamError('field'))
