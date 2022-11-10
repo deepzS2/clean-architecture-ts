cc1f9d9b1b7cace1f3ecfe4aa01e13e7410b016e
deepzS2 <alanbackman10@hotmail.com>
2022-11-10 12:16:30 -0300

    feat: ensure DbAuthentication calls UpdateAccessTokenRepository with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -2,9 +2,10 @@ import { Authentication, AuthenticationModel } from '../../../domain/usecases/au
 import { HashComparer } from '../../protocols/criptography/hash-comparer'
 import { TokenGenerator } from '../../protocols/criptography/token-generator'
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
+import { UpdateAccessTokenRepository } from '../../protocols/db/update-access-token-repository'
 
 export class DbAuthentication implements Authentication {
-  constructor (private readonly _loadAccountByEmailRepository: LoadAccountByEmailRepository, private readonly _hashComparer: HashComparer, private readonly _tokenGenerator: TokenGenerator) {}
+  constructor (private readonly _loadAccountByEmailRepository: LoadAccountByEmailRepository, private readonly _hashComparer: HashComparer, private readonly _tokenGenerator: TokenGenerator, private readonly _updateAccessTokenRepository: UpdateAccessTokenRepository) {}
 
   async auth (authentication: AuthenticationModel): Promise<string | null> {
     const account = await this._loadAccountByEmailRepository.load(authentication.email)
@@ -15,6 +16,10 @@ export class DbAuthentication implements Authentication {
 
     if (!isPasswordCorrect) return null
 
-    return await this._tokenGenerator.generate(account.id)
+    const accessToken = await this._tokenGenerator.generate(account.id)
+
+    await this._updateAccessTokenRepository.update(account.id, accessToken)
+
+    return accessToken
   }
 }
