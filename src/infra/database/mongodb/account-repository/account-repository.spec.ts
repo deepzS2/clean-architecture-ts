b1c3a28bad5df06d4006fb40e416a5202432e052
deepzS2 <alanbackman10@hotmail.com>
2022-11-10 17:59:23 -0300

    test: ensure AccountMongoRepository returns an account on loadByEmail success

--- src/infra/database/mongodb/account-repository/account-repository.spec.ts
+++ src/infra/database/mongodb/account-repository/account-repository.spec.ts
@@ -1,7 +1,9 @@
 import { MongoHelper } from '../helpers/mongo-helper'
 import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
 import { AccountMongoRepository } from './account-repository'
+import { Collection } from 'mongodb'
 
+let accountCollection: Collection
 const makeSut = (): AccountMongoRepository => new AccountMongoRepository()
 
 describe('Account Mongo Repository', () => {
@@ -14,11 +16,11 @@ describe('Account Mongo Repository', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
-  it('Should return an account on success', async () => {
+  it('Should return an account on add success', async () => {
     const sut = makeSut()
 
     const account = await sut.add({
@@ -34,5 +36,32 @@ describe('Account Mongo Repository', () => {
       email: 'any_email@mail.com',
       password: 'any_password'
     }))
+  })
+
+  it('Should return an account on loadByEmail success', async () => {
+    const sut = makeSut()
+
+    await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    const account = await sut.loadByEmail('any_email@mail.com')
+
+    expect(account).toBeTruthy()
+    expect(account?.id).toBeTruthy()
+    expect(account).toEqual(expect.objectContaining({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    }))
+  })
+
+  it('Should return null if loadByEmail fails', async () => {
+    const sut = makeSut()
+
+    const account = await sut.loadByEmail('any_email@mail.com')
+
+    expect(account).toBeFalsy()
   })
 })
