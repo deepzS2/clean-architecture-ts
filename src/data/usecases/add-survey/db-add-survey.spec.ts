a1e9f9da67fa8567df1c4177b599f946d3fe9e70
deepzS2 <alanbackman10@hotmail.com>
2022-11-21 12:33:26 -0300

    test: ensure DbAddSurvey calls AddSurveyRepository with correct values

---
+++ src/data/usecases/add-survey/db-add-survey.spec.ts
@@ -0,0 +1,47 @@
+import { it, describe, vi, expect } from 'vitest'
+import { DbAddSurvey } from './db-add-survey'
+import { AddSurveyModel, AddSurveyRepository } from './db-add-survey-protocols'
+
+interface SutTypes {
+  sut: DbAddSurvey
+  addSurveyRepositoryStub: AddSurveyRepository
+}
+
+const makeAddSurveyRepository = (): AddSurveyRepository => {
+  class AddSurveyRepositoryStub implements AddSurveyRepository {
+    async add (surveyData: AddSurveyModel): Promise<void> {
+      return await Promise.resolve()
+    }
+  }
+
+  return new AddSurveyRepositoryStub()
+}
+
+const makeSut = (): SutTypes => {
+  const addSurveyRepositoryStub = makeAddSurveyRepository()
+  const sut = new DbAddSurvey(addSurveyRepositoryStub)
+
+  return { sut, addSurveyRepositoryStub }
+}
+
+const makeFakeSurveyData = (): AddSurveyModel => ({
+  question: 'any_question',
+  answers: [
+    {
+      image: 'any_image',
+      answer: 'any_answer'
+    }
+  ]
+})
+
+describe('DbAddSurvey UseCase', () => {
+  it('Should call AddSurveyRepository with correct values', async () => {
+    const { sut, addSurveyRepositoryStub } = makeSut()
+    const addSpy = vi.spyOn(addSurveyRepositoryStub, 'add')
+    const surveyData = makeFakeSurveyData()
+
+    await sut.add(surveyData)
+
+    expect(addSpy).toHaveBeenCalledWith(surveyData)
+  })
+})
