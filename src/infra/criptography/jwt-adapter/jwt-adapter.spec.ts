5360f160138c65c528a8c83e69cd34dbcb29e186
deepzS2 <alanbackman10@hotmail.com>
2022-11-22 15:32:46 -0300

    test: ensure JwtAdapter calls verify with correct values

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -6,6 +6,9 @@ vi.mock('jsonwebtoken', () => ({
   default: {
     sign: async (): Promise<string> => {
       return await Promise.resolve('any_token')
+    },
+    verify: async (): Promise<string> => {
+      return await Promise.resolve('any_value')
     }
   }
 }))
@@ -15,31 +18,45 @@ const makeSut = (): JwtAdapter => {
 }
 
 describe('Jwt Adapter', () => {
-  it('Should call sign with correct values', async () => {
-    const id = 'any_id'
-    const sut = makeSut()
-    const signSpy = vi.spyOn(jwt, 'sign')
+  describe('Encrypt', () => {
+    it('Should call sign with correct values', async () => {
+      const id = 'any_id'
+      const sut = makeSut()
+      const signSpy = vi.spyOn(jwt, 'sign')
 
-    await sut.encrypt(id)
+      await sut.encrypt(id)
 
-    expect(signSpy).toHaveBeenCalledWith({ id }, 'secret')
-  })
-
-  it('Should return a token on sign success', async () => {
-    const sut = makeSut()
-    const accessToken = await sut.encrypt('any_id')
-
-    expect(accessToken).toBe('any_token')
-  })
-
-  it('Should call sign with correct values', async () => {
-    const sut = makeSut()
-    vi.spyOn(jwt, 'sign').mockImplementationOnce(() => {
-      throw new Error()
+      expect(signSpy).toHaveBeenCalledWith({ id }, 'secret')
     })
 
-    const promise = sut.encrypt('any_id')
+    it('Should return a token on sign success', async () => {
+      const sut = makeSut()
+      const accessToken = await sut.encrypt('any_id')
 
-    await expect(promise).rejects.toThrow()
+      expect(accessToken).toBe('any_token')
+    })
+
+    it('Should call sign with correct values', async () => {
+      const sut = makeSut()
+      vi.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+        throw new Error()
+      })
+
+      const promise = sut.encrypt('any_id')
+
+      await expect(promise).rejects.toThrow()
+    })
+  })
+
+  describe('Decrypt', () => {
+    it('Should call verify with correct values', async () => {
+      const sut = makeSut()
+
+      const verifySpy = vi.spyOn(jwt, 'verify')
+
+      await sut.decrypt('any_id')
+
+      expect(verifySpy).toHaveBeenCalledWith('any_id', 'secret')
+    })
   })
 })
