d09fb206a82b551bf39175b91d2b02f3d5c3ed69
deepzS2 <alanbackman10@hotmail.com>
2022-11-22 13:58:40 -0300

    test: ensure DbLoadAccountByToken returns null if LoadAccountByTokenRepository returns null

--- src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -70,4 +70,13 @@ describe('DbLoadAccountByToken Usecase', () => {
 
     expect(loadByTokenSpy).toHaveBeenCalledWith('any_token', 'any_role')
   })
+
+  it('Should return null if LoadAccountByTokenRepository returns null', async () => {
+    const { sut, loadAccountByTokenRepositoryStub } = makeSut()
+    vi.spyOn(loadAccountByTokenRepositoryStub, 'loadByToken').mockReturnValueOnce(Promise.resolve(null))
+
+    const account = await sut.load('any_token', 'any_role')
+
+    expect(account).toBeFalsy()
+  })
 })
