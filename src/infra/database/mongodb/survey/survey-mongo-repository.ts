c857eeed73353323ca4c0a3fd857fe4e6e00592e
deepzS2 <alanbackman10@hotmail.com>
2022-11-27 14:49:44 -0300

    feat: ensure SurveyMongoRepository load all surveys

--- src/infra/database/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/database/mongodb/survey/survey-mongo-repository.ts
@@ -1,10 +1,19 @@
 import { AddSurveyModel, AddSurveyRepository } from '../../../../data/usecases/add-survey/db-add-survey-protocols'
+import { LoadSurveysRepository, SurveyModel } from '../../../../data/usecases/load-surveys/db-load-surveys-protocols'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
   async add (surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
 
     await surveyCollection.insertOne(surveyData)
+  }
+
+  async loadAll (): Promise<SurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection<SurveyModel>('surveys')
+
+    const surveys = await surveyCollection.find().toArray()
+
+    return surveys
   }
 }
