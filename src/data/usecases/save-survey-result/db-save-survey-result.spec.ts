17635280e391af6418222a97344bbd8a2556209f
deepzS2 <alanbackman10@hotmail.com>
2022-11-29 17:19:21 -0300

    test: ensure DbSaveSurveyResult calls SaveSurveyResultRepository with correct values

---
+++ src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
@@ -0,0 +1,56 @@
+import MockDate from 'mockdate'
+import { it, describe, vi, expect, beforeAll, afterAll } from 'vitest'
+
+import { DbSaveSurveyResult } from './db-save-survey-result'
+import { SaveSurveyResultModel, SaveSurveyResultRepository, SurveyResultModel } from './db-save-survey-result-protocols'
+
+interface SutTypes {
+  sut: DbSaveSurveyResult
+  saveSurveyResultRepositoryStub: SaveSurveyResultRepository
+}
+
+const makeFakeSurveyResultData = (): SaveSurveyResultModel => ({
+  accountId: 'any_account_id',
+  surveyId: 'any_survey_id',
+  answer: 'any_answer',
+  date: new Date()
+})
+
+const makeFakeSurveyResult = (): SurveyResultModel => ({ ...makeFakeSurveyResultData(), id: 'any_id' })
+
+const makeSaveSurveyRepository = (): SaveSurveyResultRepository => {
+  class SaveSurveyResultRepositoryStub implements SaveSurveyResultRepository {
+    async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+      return await Promise.resolve(makeFakeSurveyResult())
+    }
+  }
+
+  return new SaveSurveyResultRepositoryStub()
+}
+
+const makeSut = (): SutTypes => {
+  const saveSurveyResultRepositoryStub = makeSaveSurveyRepository()
+  const sut = new DbSaveSurveyResult(saveSurveyResultRepositoryStub)
+
+  return { sut, saveSurveyResultRepositoryStub }
+}
+
+describe('DbSaveSurveyResult UseCase', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  it('Should call SaveSurveyResultRepository with correct values', async () => {
+    const { sut, saveSurveyResultRepositoryStub } = makeSut()
+    const saveSpy = vi.spyOn(saveSurveyResultRepositoryStub, 'save')
+    const surveyData = makeFakeSurveyResultData()
+
+    await sut.save(surveyData)
+
+    expect(saveSpy).toHaveBeenCalledWith(surveyData)
+  })
+})
