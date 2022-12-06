97a5154fca854479677970ccf35225fa9f396c24
deepzS2 <alanbackman10@hotmail.com>
2022-12-06 15:58:10 -0300

    fix: fixing data and domain interfaces

--- src/data/usecases/survey-result/save-survey-result/db-save-survey-result.ts
+++ src/data/usecases/survey-result/save-survey-result/db-save-survey-result.ts
@@ -6,7 +6,7 @@ import { SaveSurveyResult, SaveSurveyResultRepository } from './db-save-survey-r
 export class DbSaveSurveyResult implements SaveSurveyResult {
   constructor (private readonly _saveSurveyResultRepository: SaveSurveyResultRepository) {}
 
-  async save (data: SaveSurveyResultParams): Promise<SurveyResultModel> {
+  async save (data: SaveSurveyResultParams): Promise<SurveyResultModel | null> {
     const result = await this._saveSurveyResultRepository.save(data)
 
     return result
--- src/domain/usecases/survey-result/save-survey-result.ts
+++ src/domain/usecases/survey-result/save-survey-result.ts
@@ -3,5 +3,5 @@ import { SurveyResultModel } from '../../models/survey-result'
 export type SaveSurveyResultParams = Omit<SurveyResultModel, 'id'>
 
 export interface SaveSurveyResult {
-  save: (data: SaveSurveyResultParams) => Promise<SurveyResultModel>
+  save: (data: SaveSurveyResultParams) => Promise<SurveyResultModel | null>
 }
