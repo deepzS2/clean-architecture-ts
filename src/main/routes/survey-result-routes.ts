c76cd4f01854407b8ce1740de48b2c3882cbb6b3
deepzS2 <alanbackman10@hotmail.com>
2022-12-07 14:51:51 -0300

    feat: add no cache middleware

--- src/main/routes/survey-result-routes.ts
+++ src/main/routes/survey-result-routes.ts
@@ -2,7 +2,7 @@ import { Router } from 'express'
 
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeSaveSurveyResultController } from '../factories/controllers/survey-result/save-survey-result/save-survey-result-controller-factory'
-import { auth } from '../middlewares/auth'
+import { auth } from '../middlewares'
 
 export default (router: Router): void => {
   router.put('/surveys/:surveyId/results', auth, adaptRoute(makeSaveSurveyResultController()))
--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -3,8 +3,7 @@ import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeAddSurveyController } from '../factories/controllers/survey/add-survey/add-survey-controller-factory'
 import { makeLoadSurveysController } from '../factories/controllers/survey/load-surveys/load-surveys-controller-factory'
-import { adminAuth } from '../middlewares/admin-auth'
-import { auth } from '../middlewares/auth'
+import { auth, adminAuth } from '../middlewares'
 
 export default (router: Router): void => {
   router.post('/surveys', adminAuth, adaptRoute(makeAddSurveyController()))
