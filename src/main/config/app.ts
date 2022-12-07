e643a2eabb0f3e5d61e32d6d94549b30b733241a
deepzS2 <alanbackman10@hotmail.com>
2022-12-07 14:43:18 -0300

    feat: add swagger api

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -2,10 +2,12 @@ import express, { Express } from 'express'
 
 import setupMiddlewares from './middlewares'
 import setupRoutes from './routes'
+import setupSwagger from './swagger'
 
 export const setupApp = async (): Promise<Express> => {
   const app = express()
 
+  setupSwagger(app)
   setupMiddlewares(app)
   await setupRoutes(app)
 
---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node API',
+    description: 'Rodrigo Manguinho\'s course API to conduct polls between programmers',
+    version: '1.0.0'
+  }
+}
