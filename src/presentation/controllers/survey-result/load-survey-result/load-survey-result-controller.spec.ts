8eda1bcf8d3c86b5921ead1406df58a456fce083
deepzS2 <alanbackman10@hotmail.com>
2022-12-22 09:47:26 -0300

    test: ensure LoadSurveyResultController calls LoadSurveyById with correct id

---
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
@@ -0,0 +1,44 @@
+import MockDate from 'mockdate'
+import { describe, expect, vi, it, beforeAll, afterAll } from 'vitest'
+
+import { mockLoadSurveyById } from '@/presentation/mocks'
+
+import { LoadSurveyResultController } from './load-survey-result-controller'
+import { HttpRequest, LoadSurveyById } from './load-survey-result-protocols'
+
+interface SutTypes {
+  sut: LoadSurveyResultController
+  loadSurveyByIdStub: LoadSurveyById
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyByIdStub = mockLoadSurveyById()
+  const sut = new LoadSurveyResultController(loadSurveyByIdStub)
+
+  return { sut, loadSurveyByIdStub }
+}
+
+const mockRequest = (): HttpRequest => ({
+  params: {
+    surveyId: 'any_id'
+  }
+})
+
+describe('LoadSurveyResult Controller', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  it('Should call LoadSurveyById with correct values', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut()
+    const loadByIdSpy = vi.spyOn(loadSurveyByIdStub, 'loadById')
+
+    await sut.handle(mockRequest())
+
+    expect(loadByIdSpy).toHaveBeenCalledWith('any_id')
+  })
+})
