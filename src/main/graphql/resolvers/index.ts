f9fa22449fea827fd9970892e567b45c8371e5e6
deepzS2 <alanbackman10@hotmail.com>
2022-12-23 16:21:22 -0300

    feat: add SurveyResult query and mutation

--- src/main/graphql/resolvers/index.ts
+++ src/main/graphql/resolvers/index.ts
@@ -1,5 +1,6 @@
 import base from './base'
 import login from './login'
 import survey from './survey'
+import surveyResult from './survey-result'
 
-export default [login, survey, base]
+export default [login, survey, base, surveyResult]
---
+++ src/main/graphql/resolvers/survey-result.ts
@@ -0,0 +1,11 @@
+import { adaptResolver } from '@/main/adapters'
+import { makeLoadSurveyResultController, makeSaveSurveyResultController } from '@/main/factories'
+
+export default {
+  Query: {
+    surveyResult: async (parent: any, args: any) => await adaptResolver(makeLoadSurveyResultController(), args)
+  },
+  Mutation: {
+    saveSurveyResult: async (parent: any, args: any) => await adaptResolver(makeSaveSurveyResultController(), args)
+  }
+}
